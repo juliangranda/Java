7a9cd7530132d0bb722ed8ce2bf334370bf973e7
juliangranda <juliangranda@hotmail.com>
2024-07-30 12:43:42 -0500

    Sistema numerico: decimal,binario,octal,hexadecimal

---
+++ Variables/src/SistemasNumericos.java
@@ -0,0 +1,27 @@
+public class SistemasNumericos {
+    public static void main(String[] args) {
+
+
+        int numeroDecimal = 500;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        //decimal a binario
+        String resultadoBinario = "numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(resultadoBinario);
+
+        int numeroBinario = 0b11110;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        //decimal a octal
+        String resultadoOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(resultadoOctal);
+        int numeroOctal = 036;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        //decimal a hexadecimal
+        String resultadoHex = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(resultadoHex);
+        int numeroHex = 0x1e;
+        System.out.println("numeroHex = " + numeroHex);
+    }
+}

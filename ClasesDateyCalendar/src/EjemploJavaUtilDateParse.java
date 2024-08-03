8c2dba96a90b2d1933e20a669912f7df68155491
juliangranda <juliangranda@hotmail.com>
2024-08-03 15:39:54 -0500

    Comparar fechas

--- ClasesDateyCalendar/src/EjemploJavaUtilDateParse.java
+++ ClasesDateyCalendar/src/EjemploJavaUtilDateParse.java
@@ -15,8 +15,33 @@ public class EjemploJavaUtilDateParse {
             System.out.println("fecha = " + fecha);
             System.out.println("format = " + format.format(fecha));
 
+            Date fecha2 = new Date();
+            System.out.println("fecha2 = " + fecha2);
+
+            if (fecha.after(fecha2)) {
+                System.out.println("fecha1 (del usuario) es después que fecha2 (actual)");
+            } else if (fecha.before(fecha2)) {
+                System.out.println("fecha es anterior que fecha2");
+            } else if (fecha.equals(fecha2)) {
+                System.out.println("fecha es igual a fecha2");
+            }
+
+            if (fecha.compareTo(fecha2) > 0) {
+                System.out.println("fecha1 (del usuario) es después que fecha2 (actual)");
+            } else if (fecha.compareTo(fecha2) < 0) {
+                System.out.println("fecha es anterior que fecha2");
+            } else if (fecha.compareTo(fecha2) == 0) {
+                System.out.println("fecha es igual a fecha2");
+            }
+
+
         } catch (ParseException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.err.println("La fecha tiene un formato incorrecto: " + e.getMessage());
+            System.err.println("El formato debe ser 'yyyy-MM-dd'");
+            System.exit(1);
+            //main(args);
         }
     }
+
 }

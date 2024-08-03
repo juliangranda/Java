dd61f85549db2479515a6afa64c1d173d97b435e
juliangranda <juliangranda@hotmail.com>
2024-08-03 18:21:34 -0500

    Clase Math parte 1

---
+++ ClassMath/src/EjemploClaseMath.java
@@ -0,0 +1,24 @@
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto = " + absoluto);
+
+        absoluto = Math.abs(3);
+        System.out.println("absoluto = " + absoluto);
+
+        double max = Math.max(3.5, 1.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5, 1.2);
+        System.out.println("min = " + min);
+        //redondea el numero hacia arriba hasta ser entero.
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+        //redonde el numero hacia abajo hasta ser un entero
+        double piso = Math.floor(3.5);
+        System.out.println("piso = " + piso);
+
+        long entero = Math.round(Math.PI);
+        System.out.println("entero = " + entero);
+    }
+}

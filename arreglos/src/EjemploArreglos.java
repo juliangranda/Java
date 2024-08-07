ba9ccfe8504b45c7c48ec7a0cef57b1b2d62d3b3
juliangranda <juliangranda@hotmail.com>
2024-08-07 17:42:45 -0500

    Ejemplo de un arreglo de String y ordenas con .sort

--- arreglos/src/EjemploArreglos.java
+++ arreglos/src/EjemploArreglos.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+
 public class EjemploArreglos {
     public static void main(String[] args) {
+
+        String[] productos = new String[7];
+        productos[0] = "Kingston Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco Duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generación";
+        productos[6] = "Bicicleta Oxford";
+
+        //Ordenamos de forma alfabetica.
+        Arrays.sort(productos);
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
 
         int[] numeros = new int[4];
 

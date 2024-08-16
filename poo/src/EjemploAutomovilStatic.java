b84097d35ad3d46d2e7569476c035db413b982db
juliangranda <juliangranda@hotmail.com>
2024-08-16 16:02:43 -0500

    Atributos y metodo estaticos parte 2 - Asignacion de Id a un Objeto

--- poo/src/EjemploAutomovilStatic.java
+++ poo/src/EjemploAutomovilStatic.java
@@ -1,5 +1,6 @@
 public class EjemploAutomovilStatic {
     public static void main(String[] args) {
+        Automovil.setCapacidadEstanqueEstatico(45);
         Automovil subaru = new Automovil("Subaru", "Impresa");
 
         subaru.setCilindrada(2.0);
@@ -14,8 +15,16 @@ public class EjemploAutomovilStatic {
 
         subaru.verDetalles();
         mazda.verDetalles();
+        nissan.verDetalles();
         nissan2.verDetalles();
         System.out.println("Automovil.getColorPatente() = " + Automovil.getColorPatente());
+
+        //la forma correcta de llamar el metodo estatico es por medio de la clase
+        System.out.println("kilómetros por litros = " + Automovil.calcularConsumoEstatico(300, 60));
+
+        System.out.println("kilómetros por litros = " + subaru.calcularConsumoEstatico(300, 60));
+        System.out.println("kilómetros por litros = " + mazda.calcularConsumoEstatico(300, 60));
+        System.out.println("kilómetros por litros = " + nissan.calcularConsumoEstatico(300, 60));
 
     }
 }

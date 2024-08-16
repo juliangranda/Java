d3696ab3be2d6c8d66d1374dc546bd7b91257fdb
juliangranda <juliangranda@hotmail.com>
2024-08-16 16:22:22 -0500

    Atributos constantes final parte 2(colores)

--- poo/src/EjemploAutomovil.java
+++ poo/src/EjemploAutomovil.java
@@ -5,11 +5,13 @@ public class EjemploAutomovil {
         Automovil subaru = new Automovil("Subaru", "Impresa");
 
         subaru.setCilindrada(2.0);
-        subaru.setColor("blanco");
+        subaru.setColor(Automovil.COLOR_BLANCO);
 
-        Automovil mazda = new Automovil("Mazda", "BT-50", "Rojo", 3.0);
-        Automovil nissan = new Automovil("Nissan", "Navara", "Gris Oscuro", 3.0, 50);
-        Automovil nissan2 = new Automovil("Nissan", "Navara", "Gris Oscuro", 3.0, 50);
+        Automovil mazda = new Automovil("Mazda", "BT-50", Automovil.COLOR_ROJO, 3.0);
+        Automovil nissan = new Automovil("Nissan", "Navara", Automovil.COLOR_GRIS, 3.0, 50);
+        Automovil nissan2 = new Automovil("Nissan", "Navara", Automovil.COLOR_GRIS, 3.0, 50);
+        nissan2.setColor(Automovil.COLOR_AMARILLO);
+        Automovil.setColorPatente(Automovil.COLOR_AZUL);
         Automovil auto = new Automovil();
         Date fecha = new Date();
 

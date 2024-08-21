f3358129b630380d95c2aa1f3006b4ea2e6948ff
juliangranda <juliangranda@hotmail.com>
2024-08-21 10:18:30 -0500

    Herencia y el ApI de reflexion de java - Forma de saber quien es la clase y hija opadre

---
+++ pooHerencia/src/org/juliangranda/pooherencia/AlumnoInternacional.java
@@ -0,0 +1,22 @@
+package org.juliangranda.pooherencia;
+
+public class AlumnoInternacional extends Alumno{
+    private String pais;
+    private double notasIdiomas;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public double getNotasIdiomas() {
+        return notasIdiomas;
+    }
+
+    public void setNotasIdiomas(double notasIdiomas) {
+        this.notasIdiomas = notasIdiomas;
+    }
+}

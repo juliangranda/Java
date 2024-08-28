a3744a104a6c6efdd87511b8b186df367f4aee3e
juliangranda <juliangranda@hotmail.com>
2024-08-27 19:00:09 -0500

    Clases genericas

---
+++ Interfaces/poo_interfaces_repositorio/src/org/juliangranda/genericsclass/Camion.java
@@ -0,0 +1,30 @@
+package org.juliangranda.genericsclass;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Camion implements Iterable{
+
+    private List objetos;
+    private int max;
+
+    public Camion(int max) {
+        this.max = max;
+        this.objetos = new ArrayList();
+    }
+
+    public void add(Object objeto){
+        if(this.objetos.size() >= max){
+            this.objetos.add(objeto);
+        }else{
+            throw new RuntimeException("no hay mas espacio.");
+        }
+
+    }
+
+    @Override
+    public Iterator iterator() {
+        return this.objetos.iterator();
+    }
+}
---
+++ Interfaces/poo_interfaces_repositorio/src/org/juliangranda/genericsclass/Maquinaria.java
@@ -0,0 +1,13 @@
+package org.juliangranda.genericsclass;
+
+public class Maquinaria {
+    private String tipo;
+
+    public Maquinaria(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}

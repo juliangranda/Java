6e3ce09848830519e95e707407d2fd7a259eb0b7
juliangranda <juliangranda@hotmail.com>
2024-09-04 14:29:31 -0500

    Ejemplo TreeMap -comparacion por largo del string

---
+++ colecciones/src/org/juliangranda/ejemplo/map/EjemploTreeMap.java
@@ -0,0 +1,26 @@
+package org.juliangranda.ejemplo.map;
+
+import java.util.*;
+
+public class EjemploTreeMap {
+    public static void main(String[] args) {
+        //Comparacion por comparing pero enfocandose en el tamaño/largo del string.
+        Map<String, Object> persona = new TreeMap<>(Comparator.comparing(String::length).reversed());
+
+        persona.put("nombre", "John");
+        persona.put("apellido", "Doe");
+        persona.put("apellidoPaterno", "Doe");
+        persona.put("email", "john.doe@email.com");
+        persona.put("edad", 30);
+
+        Map<String, String> direccion = new TreeMap<>();
+        direccion.put("pais", "USA");
+        direccion.put("estado", "California");
+        direccion.put("ciudad", "Santa Barbara");
+        direccion.put("calle", "One Street");
+        direccion.put("numero", "120");
+
+        persona.put("direccion", direccion);
+        System.out.println("persona = " + persona);
+    }
+}

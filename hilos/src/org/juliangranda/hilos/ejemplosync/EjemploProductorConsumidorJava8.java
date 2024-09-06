4171b8cb897be90a67a9747f6fecb2c9a7a209f5
juliangranda <juliangranda@hotmail.com>
2024-09-06 17:13:29 -0500

    Ejemplo sincronizacion Productor Consumidor con java 8 lambda

---
+++ hilos/src/org/juliangranda/hilos/ejemplosync/EjemploProductorConsumidorJava8.java
@@ -0,0 +1,29 @@
+package org.juliangranda.hilos.ejemplosync;
+
+import org.juliangranda.hilos.ejemplosync.runnable.Consumidor;
+import org.juliangranda.hilos.ejemplosync.runnable.Panadero;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class EjemploProductorConsumidorJava8 {
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+
+        new Thread( () -> {
+            for (int i = 0; i < 10; i++) {
+                p.hornear("Pan n "+i);
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(500,2000));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        new Thread( () -> {
+            for (int i = 0; i < 10; i++) {
+                p.consumir();
+            }
+        }).start();
+    }
+
+}

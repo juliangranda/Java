a33ce461bbf845912932c198821459528d274edb
juliangranda <juliangranda@hotmail.com>
2024-09-08 15:57:47 -0500

    Creando nuestra propia expresion lambda con una interfaz funcional

---
+++ java8_lambda/src/org/juliangranda/java8/lambda/aritmetica/Aritmetica.java
@@ -0,0 +1,8 @@
+package org.juliangranda.java8.lambda.aritmetica;
+
+@FunctionalInterface
+public interface Aritmetica {
+
+    //@FuntionalInterface significa que solo tiene un metodo abstracto.
+    double operacion(double a, double b);
+}

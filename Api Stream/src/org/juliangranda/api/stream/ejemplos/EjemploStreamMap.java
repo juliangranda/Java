086d84caa79b676f41992e63b58164951f248b15
juliangranda <juliangranda@hotmail.com>
2024-09-09 18:06:21 -0500

    el operador map y peek - Streams

---
+++ Api Stream/src/org/juliangranda/api/stream/ejemplos/EjemploStreamMap.java
@@ -0,0 +1,43 @@
+package org.juliangranda.api.stream.ejemplos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EjemploStreamMap {
+    public static void main(String[] args) {
+
+//        Stream<String> nombres = Stream
+//                .of("Pato","Paco","Pepa","Pepe")
+//                .map(nombre -> {
+//                    return nombre.toUpperCase();
+//                })
+//                .peek(e -> { System.out.println(e); })
+//                .map( e -> e.toLowerCase())
+//                ;
+
+//        peek(se usa para debugging o rastrear el stream),
+//        para visualizar o saber como está quedando el stream
+//        después de cada transformation u operación en éste,
+//        ademas el peek() debe de ir después de dicha operación.
+
+
+        Stream<String> nombres = Stream
+                .of("Pato","Paco","Pepa","Pepe")
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .map(String::toLowerCase)
+                ;
+
+
+        //Convertir el Stream a una Lista
+        List<String> lista = nombres.collect(Collectors.toList());
+        lista.forEach(System.out::println);
+
+        //Steam no se ejecuta hasta que se invoca un metodo final( forEach ) u otro.
+        //nombres.forEach(System.out::println);
+
+
+    }
+}

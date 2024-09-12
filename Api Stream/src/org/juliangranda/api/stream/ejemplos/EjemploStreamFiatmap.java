5d14d3246b7165212b16f96bb7689d6bcdbe4562
juliangranda <juliangranda@hotmail.com>
2024-09-12 11:05:43 -0500

    renombrar archivos

--- Api Stream/src/org/juliangranda/api/stream/ejemplos/EjemploStreamFiatmap.java
+++ Api Stream/src/org/juliangranda/api/stream/ejemplos/EjemploStreamFiatmap.java
@@ -2,29 +2,27 @@ package org.juliangranda.api.stream.ejemplos;
 
 import org.juliangranda.api.stream.ejemplos.models.Usuario;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
-public class EjemploStreamFilter {
+public class EjemploStreamFiatmap {
     public static void main(String[] args) {
 
         Stream<Usuario> nombres = Stream
                 .of("Pato Guzman","Paco Gonzalez","Pepa Gutierrez ","Pepe Mena","Pepe Garcia")
                 //posicion 0 el nombre, posicion 1 el apellido.
                 .map(nombre -> new Usuario(nombre.split(" ")[0],nombre.split(" ")[1]))
-                .filter( u -> u.getNombre().equals("Pepe"))
-                .peek(System.out::println)
-                .map(usuario -> {
-                    String nombre = usuario.getNombre().toUpperCase();
-                    usuario.setNombre(nombre);
-                    return usuario;
+                //flatMap: devuelve o retorna varias salidas por cada elemento,devuelve un stream separado.
+                .flatMap( u -> {
+                    if(u.getNombre().equalsIgnoreCase("Pepe")){
+                        return Stream.of(u);
+                    }
+                    return Stream.empty();
                 })
+                .peek(System.out::println)
                 ;
 
-
-        List<Usuario> lista = nombres.toList();
-        lista.forEach(System.out::println);
+        //nombres.forEach(System.out::println);
+        System.out.println(nombres.count());
 
 
     }

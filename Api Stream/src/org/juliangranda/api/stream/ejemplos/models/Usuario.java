3ebdddb3de2ae2db4f6fb82a5aaf09b7327fa443
juliangranda <juliangranda@hotmail.com>
2024-09-12 12:15:36 -0500

    Ejemplo flatMap facturas de usuarios

--- Api Stream/src/org/juliangranda/api/stream/ejemplos/models/Usuario.java
+++ Api Stream/src/org/juliangranda/api/stream/ejemplos/models/Usuario.java
@@ -1,5 +1,7 @@
 package org.juliangranda.api.stream.ejemplos.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Usuario {
@@ -9,10 +11,24 @@ public class Usuario {
     private Integer id;
     private static int ultimoId;
 
+    private List<Factura> facturas;
+
     public Usuario(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.id = ++ultimoId;
+        this.facturas = new ArrayList<>();
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void addFactura(Factura factura) {
+        this.facturas.add(factura);
+        //relacion en ambos sentidos
+        //al añadir la factura tambien añade el usuario
+        factura.setUsuario(this);
     }
 
     public Integer getId() {

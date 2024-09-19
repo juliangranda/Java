754c30b95eb93e949902f14a3bca9ee2813e2fb8
juliangranda <juliangranda@hotmail.com>
2024-09-18 19:05:55 -0500

    Trabajando con LocalDate con fechas

---
+++ java8DateTime/src/org/juliangranda/datetime/ejemplos/EjemploLocalDate.java
@@ -0,0 +1,56 @@
+package org.juliangranda.datetime.ejemplos;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class EjemploLocalDate {
+    public static void main(String[] args) {
+
+        LocalDate fechaActual = LocalDate.now();
+        System.out.println("fechaActual = " + fechaActual);
+        System.out.println("Dia: " + fechaActual.getDayOfMonth());
+        System.out.println("Mes: " + fechaActual.getMonth());
+        System.out.println("Año: " + fechaActual.getYear());
+        System.out.println("Dia del año: " + fechaActual.getDayOfYear());
+        System.out.println("Era: " + fechaActual.getEra());
+
+        fechaActual = LocalDate.of(2020,12,25);
+        System.out.println(fechaActual);
+
+        fechaActual = LocalDate.of(2020, Month.NOVEMBER, 11);
+        System.out.println("fechaActual = " + fechaActual);
+
+        fechaActual = LocalDate.parse("2020-02-01");
+        System.out.println("fechaActual = " + fechaActual);
+
+        LocalDate diaDeMañana = LocalDate.now().plusDays(1);
+        System.out.println("diaDeMañana = " + diaDeMañana);
+
+        LocalDate mesAnteriorMismoDia = LocalDate.now().minus(1, ChronoUnit.MONTHS);
+        System.out.println("mesAnteriorMismoDia = " + mesAnteriorMismoDia);
+
+        DayOfWeek miercoles = LocalDate.parse("2020-11-11").getDayOfWeek();
+        System.out.println("miercoles = " + miercoles);
+
+        int once = LocalDate.of(2020,11,11).getDayOfMonth();
+        System.out.println("once = " + once);
+
+        //bisiesto el año o no.
+        boolean esBisiesto = LocalDate.now().isLeapYear();
+        System.out.println("esBisiesto = " + esBisiesto);
+
+        boolean isAntes = LocalDate.of(2020,11,11).isBefore(LocalDate.parse("2020-11-10"));
+        System.out.println("isAntes = " + isAntes);
+
+        boolean esDespues = LocalDate.parse("2020-11-11").isAfter(LocalDate.parse("2020-11-10"));
+        System.out.println("esDespues = " + esDespues);
+
+        esDespues = LocalDate.now().isAfter(LocalDate.now().minusDays(1));
+        System.out.println("esDespues = " + esDespues);
+
+
+
+    }
+}

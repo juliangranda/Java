d27e111bf77e175e7802a45ed5c5e0590db81822
juliangranda <juliangranda@hotmail.com>
2024-09-19 18:11:12 -0500

    Trabajando con Zonas Horarios usando ZoneDateTime parte 2

--- java8DateTime/src/org/juliangranda/datetime/ejemplos/EjemploZoneDateTime.java
+++ java8DateTime/src/org/juliangranda/datetime/ejemplos/EjemploZoneDateTime.java
@@ -2,19 +2,37 @@ package org.juliangranda.datetime.ejemplos;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
 
 public class EjemploZoneDateTime {
     public static void main(String[] args) {
 
-        LocalDateTime fechaLocal = LocalDateTime.now();
+        LocalDateTime fechaLocal = LocalDateTime.parse("2021/09/23 12:45"
+                , DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
         //cada pais tiene una Zone id buscar en wikipedia u otro.
-        ZoneId newyork = ZoneId.of("America/New_York");
-        ZonedDateTime zonaNy = ZonedDateTime.of(fechaLocal,newyork);
+        //ZonedDateTime zonaNy = ZonedDateTime.of(fechaLocal, ZoneOffset.of("-04:00"));
+
+        //sin hacer uso del offset se usa ZoneId
+        //ZoneId newyork = ZoneId.of("America/New_York");
+
+        //haciendo uso del offset o utc(tiempo universal coordinado)
+        ZonedDateTime zonaNy = fechaLocal.atZone(ZoneOffset.of("-04:00"));
         System.out.println("Hora de partida new york: " + zonaNy);
 
-        ZoneId madrid = ZoneId.of("Europe/Madrid");
-        ZonedDateTime zonaMadrid = zonaNy.withZoneSameInstant(madrid).plusHours(8);
+        //ZoneId madrid = ZoneId.of("Europe/Madrid");
+        ZonedDateTime zonaMadrid = zonaNy.withZoneSameInstant(ZoneOffset.of("+02:00")).plusHours(8);
         System.out.println("hora llegada madrid: " + zonaMadrid);
+
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm, dd MMM yyyy");
+        System.out.println("Detalles del viaje a españa");
+        System.out.println("Partida NY: " + f.format(zonaNy));
+        System.out.println("LLegada Madrid: " + f.format(zonaMadrid));
+
+        //Muestra todas las ZOneIds disponibles.
+        ZoneId.getAvailableZoneIds().forEach(System.out::println);
+
     }
 }

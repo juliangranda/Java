3fc3e595e076253602358583dacf11fe16c64b40
juliangranda <juliangranda@hotmail.com>
2024-09-22 17:32:22 -0500

    Patron Abstract Factory

---
+++ PatronesDisenio/src/org/juliangranda/patrones/factory/PizzaProducto.java
@@ -0,0 +1,42 @@
+package org.juliangranda.patrones.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PizzaProducto {
+
+    protected String nombre;
+    protected String masa;
+    protected String salsa;
+    protected List<String> ingredientes;
+
+    public PizzaProducto() {
+        this.ingredientes = new ArrayList<>();
+    }
+
+    public void preparar(){
+        System.out.println("Preparando " + nombre);
+        System.out.println("Seleccionado la masa "+ masa);
+        System.out.println("Agregando la salsa " + salsa);
+        System.out.println("Agrgando los ingredientes " );
+        this.ingredientes.forEach(System.out::println);
+
+    }
+
+    abstract public void cocinar();
+    abstract void cortar();
+
+    public void empaquetar(){
+        System.out.println("Poniendo la pizza en una caja de empaque");
+    }
+
+    @Override
+    public String toString() {
+        return "PizzaProducto{" +
+                "nombre='" + nombre + '\'' +
+                ", masa='" + masa + '\'' +
+                ", salsa='" + salsa + '\'' +
+                ", ingredientes=" + ingredientes +
+                '}';
+    }
+}

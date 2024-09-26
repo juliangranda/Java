7bb27690d6e1e7e400b6417878a3dc021690d393
juliangranda <juliangranda@hotmail.com>
2024-09-26 15:16:52 -0500

    Cerrando conexiones,recursos y manejo de errores

--- java_jdbc/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
+++ java_jdbc/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
@@ -9,19 +9,23 @@ public class EjemploJDBC {
         String url = "jdbc:mysql://localhost:3306/java_curso?serverTimeZone=UTC";
         String username = "root";
         String password ="admin";
-        try {
-            Connection conn = DriverManager.getConnection(url,username,password);
-            Statement stmt = conn.createStatement();
-            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+
+        try(Connection conn = DriverManager.getConnection(url,username,password);
+        Statement stmt = conn.createStatement();
+        ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");) {
 
             while(resultado.next()){
+                System.out.print(resultado.getInt("id"));
+                System.out.print(" | ");
+                System.out.print(resultado.getInt("precio"));
+                System.out.print(" | ");
+                System.out.print(resultado.getString("fecha_registro"));
+                System.out.print(" | ");
                 System.out.println(resultado.getString("nombre"));
             }
-            resultado.close();
-            stmt.close();
-            conn.close();
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }

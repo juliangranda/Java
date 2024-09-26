1cb93d7ccd840ebdb049706d3f46614ff8a15c1b
juliangranda <juliangranda@hotmail.com>
2024-09-26 17:09:40 -0500

    Implementando la clase Repositorio parte 3

--- java_jdbc/src/main/java/org/juliangranda/java/jdbc/repositorio/ProductoRepositorioImpl.java
+++ java_jdbc/src/main/java/org/juliangranda/java/jdbc/repositorio/ProductoRepositorioImpl.java
@@ -37,16 +37,55 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
         try(PreparedStatement stmt = getConnection()
                 .prepareStatement("SELECT * FROM productos WHERE id = ? ")){
             stmt.setLong(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                producto = crearProducto(rs);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    producto = crearProducto(rs);
+                }
             }
-            rs.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return producto;
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        String sql;
+        if (producto.getId() != null && producto.getId()>0) {
+            sql = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre,precio,fecha_registro) VALUES(?,?,?)";
+        }
+        try(PreparedStatement stmt = getConnection().prepareStatement(sql)){
+            stmt.setString(1, producto.getNombre());
+            stmt.setLong(2,producto.getPrecio());
+
+            if (producto.getId() != null && producto.getId()>0) {
+                stmt.setLong(3,producto.getId());
+            }else{
+                //hay conversion de java.util a java.sql,
+                // pero no es necesario cuando es java.sql a java.util porque el sql hereda de java.util.
+                //esto es por insertar datos desde java a sql-basedatos.
+                stmt.setDate(3,new Date(producto.getFechaRegistro().getTime()));
+            }
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        try(PreparedStatement stmt = getConnection().prepareStatement("SELECT * FROM productos WHERE id=?")){
+            stmt.setLong(1,id);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Producto crearProducto(ResultSet rs) throws SQLException {
@@ -56,15 +95,5 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
         p.setPrecio(rs.getInt("precio"));
         p.setFechaRegistro(rs.getDate("fecha_registro"));
         return p;
-    }
-
-    @Override
-    public void guardar(Producto producto) {
-
-    }
-
-    @Override
-    public void eliminar(Long id) {
-
     }
 }

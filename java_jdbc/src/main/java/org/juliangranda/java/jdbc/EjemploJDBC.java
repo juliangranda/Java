63d485b322548fc46641c8cd402300d5f75d4af1
juliangranda <juliangranda@hotmail.com>
2024-09-26 15:28:17 -0500

    Añadiendo la clase Singleton de conexion de bases de datos

--- java_jdbc/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
+++ java_jdbc/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
@@ -1,18 +1,17 @@
 package org.juliangranda.java.jdbc;
 
+import org.juliangranda.java.jdbc.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 //https://mvnrepository.com/   importante para dependencias
 public class EjemploJDBC {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimeZone=UTC";
-        String username = "root";
-        String password ="admin";
 
-        try(Connection conn = DriverManager.getConnection(url,username,password);
-        Statement stmt = conn.createStatement();
-        ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");) {
+        try(Connection conn = ConexionBaseDatos.getInstance();
+            Statement stmt = conn.createStatement();
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");) {
 
             while(resultado.next()){
                 System.out.print(resultado.getInt("id"));

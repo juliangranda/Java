6f78198db1b943da0658314d113a05a9018c20fd
juliangranda <juliangranda@hotmail.com>
2024-09-27 18:45:08 -0500

    Ejemplo Transacciones con pool de conexiones

---
+++ java_jdbc_pool_trx/src/main/java/org/juliangranda/java/jdbc/model/Producto.java
@@ -0,0 +1,88 @@
+package org.juliangranda.java.jdbc.model;
+
+import java.util.Date;
+
+public class Producto {
+    private Long id;
+    private String nombre;
+    private int precio;
+    private Date fechaRegistro;
+    private Categoria categoria;
+    private String sku;
+
+
+    public Producto() {
+
+    }
+
+    public Producto(Long id, String nombre, int precio, Date fechaRegistro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.fechaRegistro = fechaRegistro;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    @Override
+    public String toString() {
+        return
+                id +
+                " | " +
+                nombre +
+                " | " +
+                precio +
+                " | " +
+                fechaRegistro +
+                " | " +
+                categoria.getNombre()
+                + " | " + sku;
+    }
+}
---
+++ java_jdbc_pool_trx/src/main/java/org/juliangranda/java/jdbc/repositorio/Repositorio.java
@@ -0,0 +1,17 @@
+package org.juliangranda.java.jdbc.repositorio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+//tema de consulta y operaciones de la base de datos.
+//CRUD - DAO/Repositorio.
+public interface Repositorio<T> {
+    List<T> listar() throws SQLException;
+
+    T porId(Long id) throws SQLException;
+
+    T guardar(T t) throws SQLException;
+
+    void eliminar(Long id) throws SQLException;
+
+}

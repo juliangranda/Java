e494f5df4cc4824eb16c8cc50a32d5e3f119138d
juliangranda <juliangranda@hotmail.com>
2024-09-28 16:35:03 -0500

    creando y configurando el proyecto con junit5

---
+++ junit5_app/src/main/java/org/juliangranda/junit5/ejemplo/models/Cuenta.java
@@ -0,0 +1,24 @@
+package org.juliangranda.junit5.ejemplo.models;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+    private String persona;
+    private BigDecimal saldo;
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}

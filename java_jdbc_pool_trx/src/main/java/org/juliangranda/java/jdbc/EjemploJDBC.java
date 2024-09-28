b387b78d1efa8244b443526f8fbe8125caeeb8ca
juliangranda <juliangranda@hotmail.com>
2024-09-28 11:45:25 -0500

    Ejemplo Transaccionescon pool de conexiones parte 4

--- java_jdbc_pool_trx/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
+++ java_jdbc_pool_trx/src/main/java/org/juliangranda/java/jdbc/EjemploJDBC.java
@@ -2,34 +2,54 @@ package org.juliangranda.java.jdbc;
 
 import org.juliangranda.java.jdbc.model.Categoria;
 import org.juliangranda.java.jdbc.model.Producto;
+import org.juliangranda.java.jdbc.repositorio.CategoriaRepositorioImpl;
 import org.juliangranda.java.jdbc.repositorio.ProductoRepositorioImpl;
 import org.juliangranda.java.jdbc.repositorio.Repositorio;
+import org.juliangranda.java.jdbc.util.ConexionBaseDatos;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 
 public class EjemploJDBC {
     public static void main(String[] args) throws SQLException {
 
-//        Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
-//        System.out.println("==========listar==============");
-//        repositorio.listar().forEach(System.out::println);
-//
-//        System.out.println("==========obtener por id==============");
-//        System.out.println(repositorio.porId(1L));
+        try(Connection conn = ConexionBaseDatos.getConnection();) {
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                Repositorio<Categoria> repositorioCategoria = new CategoriaRepositorioImpl(conn);
+                System.out.println("============ Insertar nueva categoria: ============");
+                Categoria categoria = new Categoria();
+                categoria.setNombre("Electrohogar");
+                Categoria nuevacategoria = repositorioCategoria.guardar(categoria);
+                System.out.println("Categoria guardado con exito: " + nuevacategoria.getId());
 
-//        System.out.println("========== Insertar nuevo producto ==============");
-//        Producto producto = new Producto();
-//        producto.setNombre("HP");
-//        producto.setPrecio(600);
-//        producto.setFechaRegistro(new Date());
-//        Categoria categoria = new Categoria();
-//        categoria.setId(3L);
-//        producto.setCategoria(categoria);
-//        //repositorio.guardar(producto);
-//        System.out.println("Producto guardado con exito");
+                Repositorio<Producto> repositorio = new ProductoRepositorioImpl(conn);
+                System.out.println("==========listar==============");
+                repositorio.listar().forEach(System.out::println);
 
-        //repositorio.listar().forEach(System.out::println);
+                System.out.println("==========obtener por id==============");
+                System.out.println(repositorio.porId(1L));
 
+                System.out.println("========== Insertar nuevo producto ==============");
+                Producto producto = new Producto();
+                producto.setNombre("Refrigerador samsung");
+                producto.setPrecio(9900);
+                producto.setFechaRegistro(new Date());
+                producto.setSku("abcdefg123");
+
+                producto.setCategoria(nuevacategoria);
+                repositorio.guardar(producto);
+                System.out.println("Producto guardado con exito: " + producto.getId());
+
+                repositorio.listar().forEach(System.out::println);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                throw new RuntimeException(e);
+            }
+        }
     }
 }

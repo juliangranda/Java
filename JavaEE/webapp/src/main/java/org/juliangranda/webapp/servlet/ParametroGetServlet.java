400aaf6ec987a36093f95581f2e52f8db8044393
juliangranda <juliangranda@hotmail.com>
2024-10-10 16:54:11 -0500

    Enviando multiples parametros GET url

--- JavaEE/webapp/src/main/java/org/juliangranda/webapp/servlet/ParametroGetServlet.java
+++ JavaEE/webapp/src/main/java/org/juliangranda/webapp/servlet/ParametroGetServlet.java
@@ -17,6 +17,7 @@ public class ParametroGetServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         String saludo = req.getParameter("saludo");
+        String nombre = req.getParameter("nombre");
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("    <head>");
@@ -25,7 +26,22 @@ public class ParametroGetServlet extends HttpServlet {
         out.println("    </head>");
         out.println("    <body>");
         out.println("    <h1>parametros Get de la url</h1>");
-        out.println("    <h2>El saludo enviado es:" + saludo + "</h2>");
+        if(saludo != null && nombre != null){
+            out.println("    <h2>El saludo enviado es: " + saludo + " " + nombre + "</h2>");
+        }else if(saludo != null){
+            out.println("    <h2>El saludo enviado es: " + saludo + "</h2>");
+        } else if (nombre != null) {
+            out.println("    <h2>Hola mi nombre es: " + nombre + "</h2>");
+        } else{
+            out.println("<h2>no se han pasado los parametros saludo ni nombre");
+        }
+        try{
+            int codigo = Integer.parseInt(req.getParameter("codigo"));
+            out.println("<h3>El codigo enviado es : " + codigo + "</h3>");
+        }catch(NumberFormatException e){
+            out.println("<h3>El codigo no ha sido enviado, es null</h3>");
+        }
+
         out.println("    </body>");
         out.println("</html>");
         out.close();

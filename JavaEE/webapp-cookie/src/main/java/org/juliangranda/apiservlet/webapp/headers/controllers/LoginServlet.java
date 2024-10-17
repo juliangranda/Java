50ec2a6a72b1a241b9df429bac5bc7b5332ff466
juliangranda <juliangranda@hotmail.com>
2024-10-17 11:51:28 -0500

    Sistema Login con cookies parte 3 cerrando session logout - evitando que la cookie se mantenga en el cache

--- JavaEE/webapp-cookie/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/LoginServlet.java
+++ JavaEE/webapp-cookie/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/LoginServlet.java
@@ -36,8 +36,9 @@ public class LoginServlet extends HttpServlet {
                 out.println("        <title>Hola " + cookieOptional.get() + "</title>");
                 out.println("    </head>");
                 out.println("    <body>");
-                out.println("    <h1>Hola " + cookieOptional.get() + " ya has iniciado session anteriormente</h1>");
+                out.println("    <h1>Hola " + cookieOptional.get() + " has iniciado session con éxito</h1>");
                 out.println("    <p><a href='" + req.getContextPath() + "/index.html'>volver</a></p>");
+                out.println("    <p><a href='" + req.getContextPath() + "/logout'>cerrar sesión</a></p>");
                 out.println("    </body>");
                 out.println("</html>");
             }
@@ -59,22 +60,7 @@ public class LoginServlet extends HttpServlet {
             Cookie usernameCookie = new Cookie("username",username);
             resp.addCookie(usernameCookie);
 
-            resp.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = resp.getWriter()) {
-
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("    <head>");
-                out.println("        <meta charset=\"UTF-8\">");
-                out.println("        <title>Login Correcto</title>");
-                out.println("    </head>");
-                out.println("    <body>");
-                out.println("    <h1>Login Correcto</h1>");
-                out.println("        </h3>Hola " + username + " has iniciado con exito </h3>");
-                out.println("<p><a href='" + req.getContextPath() + "/index.html'>volver</a></p>");
-                out.println("    </body>");
-                out.println("</html>");
-            }
+            resp.sendRedirect(req.getContextPath() + "/login.html");
 
         }else{
             resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Lo sentimos no esta autorizado para ingresar a esta pagina");

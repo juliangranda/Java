1a511fac2557693cae2fba01065f3703fee5a133
juliangranda <juliangranda@hotmail.com>
2024-10-18 18:32:25 -0500

    Implementando un filtro que valida la session de usuario o login

---
+++ JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/filters/LoginFiltro.java
@@ -0,0 +1,30 @@
+package org.juliangranda.apiservlet.webapp.headers.filters;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.juliangranda.apiservlet.webapp.headers.services.LoginService;
+import org.juliangranda.apiservlet.webapp.headers.services.LoginServiceSessionImpl;
+
+import java.io.IOException;
+import java.util.Optional;
+
+//falta la parte de actualizar-carro es complemento opcional
+//@WebFilter({"/ver-carro","/agregar-carro","actualizar-carro"})
+//@WebFilter({"/ver-carro","/agregar-carro"})
+@WebFilter({"/carro/*"})
+public class LoginFiltro implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LoginService service = new LoginServiceSessionImpl();
+        Optional<String> username = service.getUsername((HttpServletRequest) request);
+        if (username.isPresent()) {
+            chain.doFilter(request,response);
+        }else{
+            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                    "Lo sentimos no estas autorizado para ingresar a esta pagina");
+        }
+    }
+}

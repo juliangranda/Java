f29d3ccaa7eb6202b530d425bd09e4216f36fab7
juliangranda <juliangranda@hotmail.com>
2024-10-25 12:36:39 -0500

    Modificando el listado de productos con vista JSP Y MVC

--- JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/ProductoServlet.java
+++ JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/ProductoServlet.java
@@ -19,7 +19,6 @@ import java.util.Optional;
 public class ProductoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         Connection conn = (Connection) req.getAttribute("conn");
         ProductoService service = new ProductoServiceJdbcImpl(conn);
         List<Producto> productos = service.listar();
@@ -27,52 +26,8 @@ public class ProductoServlet extends HttpServlet {
         LoginService auth = new LoginServiceSessionImpl();
         Optional<String> usernameOptional = auth.getUsername(req);
 
-        String mensajeRequest = (String) req.getAttribute("mensaje");
-        String mensajeApp = (String) getServletContext().getAttribute("mensaje");
-        resp.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = resp.getWriter()) {
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("    <head>");
-            out.println("        <meta charset=\"UTF-8\">");
-            out.println("        <title>Listado de Productos</title>");
-            out.println("    </head>");
-            out.println("    <body>");
-            out.println("    <h1>Listado de Productos</h1>");
-            if(usernameOptional.isPresent()) {
-                out.println("<div style='color: blue;'>Hola " + usernameOptional.get() + " Bienvenido</div>");
-            }
-            out.println("<table>");
-            out.println("<tr>");
-            out.println("<th>id</th>");
-            out.println("<th>nombre</th>");
-            out.println("<th>tipo</th>");
-            if(usernameOptional.isPresent()) {
-                out.println("<th>precio</th>");
-                out.println("<th>agregar</th>");
-            }
-            out.println("</tr>");
-            productos.forEach(p -> {
-                out.println("<tr>");
-                out.println("<td>" + p.getId() + "</td>");
-                out.println("<td>" + p.getNombre() + "</td>");
-                out.println("<td>" + p.getTipo() + "</td>");
-                if(usernameOptional.isPresent()){
-                    out.println("<td>" + p.getPrecio() + "</td>");
-                    out.println("<td><a href=\""
-                            + req.getContextPath()
-                            + "/carro/agregar?id="+ p.getId()
-                            + "\">agregar al carro</a></td>");
-                }
-                out.println("</tr>");
-            });
-            out.println("</table>");
-            out.println("<p>" + mensajeApp + "</p>");
-            out.println("<p>" + mensajeRequest + "</p>");
-            out.println("    </body>");
-            out.println("</html>");
-
-        }
+        req.setAttribute("productos",productos);
+        req.setAttribute("username", usernameOptional);
+        getServletContext().getRequestDispatcher("/listar.jsp").forward(req,resp);
     }
 }

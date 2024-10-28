d5ab2a4fb800fe726fca290e696d8807a2fa679a
juliangranda <juliangranda@hotmail.com>
2024-10-28 11:33:46 -0500

    Procesando el formulario con doPost para crear

--- JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/ProductoFormServlet.java
+++ JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/controllers/ProductoFormServlet.java
@@ -5,11 +5,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.juliangranda.apiservlet.webapp.headers.models.Categoria;
+import org.juliangranda.apiservlet.webapp.headers.models.Producto;
 import org.juliangranda.apiservlet.webapp.headers.services.ProductoService;
 import org.juliangranda.apiservlet.webapp.headers.services.ProductoServiceJdbcImpl;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet("/productos/form")
 public class ProductoFormServlet extends HttpServlet {
@@ -25,5 +29,38 @@ public class ProductoFormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        Connection conn = (Connection) req.getAttribute("conn");
+        ProductoService service = new ProductoServiceJdbcImpl(conn);
+        String nombre = req.getParameter("nombre");
+
+        Integer precio;
+        try {
+            precio = Integer.valueOf(req.getParameter("precio"));
+        } catch (NumberFormatException e){
+            precio = 0;
+        }
+
+        String sku = req.getParameter("sku");
+        String fechaStr = req.getParameter("fecha_registro");
+        Long categoriaId;
+        try{
+            categoriaId = Long.valueOf(req.getParameter("ccategoria"));
+        }catch(NumberFormatException e){
+            categoriaId = 0L;
+        }
+
+        LocalDate fecha = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Producto producto = new Producto();
+        producto.setSku(nombre);
+        producto.setSku(sku);
+        producto.setPrecio(precio);
+        producto.setFechaRegistro(fecha);
+
+        Categoria categoria = new Categoria();
+        categoria.setId(categoriaId);
+        producto.setCategoria(categoria);
+
+        service.guardar(producto);
+        resp.sendRedirect(req.getContextPath() + "/productos");
     }
 }

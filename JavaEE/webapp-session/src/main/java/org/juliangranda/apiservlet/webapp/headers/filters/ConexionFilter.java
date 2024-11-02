cbd9e98ef76c144e2723669537cafcff52f04e18
juliangranda <juliangranda@hotmail.com>
2024-11-02 18:31:42 -0500

    Manejo de transacciones usando Interceptores y anotaciones

--- JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ JavaEE/webapp-session/src/main/java/org/juliangranda/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -14,32 +14,34 @@ import java.sql.SQLException;
 @WebFilter("/*")
 public class ConexionFilter implements Filter {
 
-    @Inject
+    /*@Inject
     @MysqlConn
-    private Connection conn;
+    private Connection conn;*/
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        try {
+        /*try {
             Connection connRequest = this.conn;
 
             if (connRequest.getAutoCommit()) {
                 connRequest.setAutoCommit(false);
             }
+
+         */
             try {
                 //guardar la conexion de la base de datos.
                 //request.setAttribute("conn",connRequest);
                 //doFilter es cuando se devuelve la llamada a un doPost o doGet
                 chain.doFilter(request, response);
-                connRequest.commit();
-            }catch (SQLException | ServiceJdbcException e){
-                connRequest.rollback();
+                //connRequest.commit();
+            } catch (ServiceJdbcException e){
+                //connRequest.rollback();
                 ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
                 throw  new RuntimeException(e);
             }
-        } catch (SQLException e) {
+        /*} catch (SQLException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 }

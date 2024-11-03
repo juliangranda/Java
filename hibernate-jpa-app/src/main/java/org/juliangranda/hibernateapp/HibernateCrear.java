781077fc5609d27deb67158a2cef231f72cd1294
juliangranda <juliangranda@hotmail.com>
2024-11-03 14:38:52 -0500

    Crear registros con el metodo persist del EntityManager

---
+++ hibernate-jpa-app/src/main/java/org/juliangranda/hibernateapp/HibernateCrear.java
@@ -0,0 +1,36 @@
+package org.juliangranda.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.juliangranda.hibernateapp.entity.Cliente;
+import org.juliangranda.hibernateapp.util.JpaUtil;
+
+import javax.swing.*;
+
+public class HibernateCrear {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+        try{
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre:");
+            String apellido = JOptionPane.showInputDialog("Ingrese el apellido:");
+            String pago = JOptionPane.showInputDialog("Ingrese la foma de pago:");
+            em.getTransaction().begin();
+
+            Cliente c = new Cliente();
+            c.setNombre(nombre);
+            c.setApellido(apellido);
+            c.setFormaPago(pago);
+            em.persist(c);
+            em.getTransaction().commit();
+
+            System.out.println("el id del cliente registrado es:" + c.getId());
+            c = em.find(Cliente.class, c.getId());
+            System.out.println(c);
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+    }
+}

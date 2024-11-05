6eeab5c1e9b4797962d91739ed8ea2c526257889
juliangranda <juliangranda@hotmail.com>
2024-11-05 17:21:31 -0500

    Eliminar objetos dependientes en OneToMany Bidireccional

--- hibernate-jpa-asociaciones-app/src/main/java/org/juliangranda/hibernateapp/HibernateAsociacionesOneToManyBidireccionalFind.java
+++ hibernate-jpa-asociaciones-app/src/main/java/org/juliangranda/hibernateapp/HibernateAsociacionesOneToManyBidireccionalFind.java
@@ -25,6 +25,15 @@ public class HibernateAsociacionesOneToManyBidireccionalFind {
 //            em.merge(cliente);
             em.getTransaction().commit();
             System.out.println(cliente);
+
+            em.getTransaction().begin();
+            //Factura f3 = em.find(Factura.class,1L); //alternativa
+            Factura f3 = new Factura("compras de supermercado", 5000L);
+            f3.setId(1L);
+            //eliminar factura en ambos lados
+            cliente.removeFactura(f3);
+            em.getTransaction().commit();
+            System.out.println(cliente);
         }catch(Exception e){
             em.getTransaction().rollback();
             e.printStackTrace();
--- hibernate-jpa-asociaciones-app/src/main/java/org/juliangranda/hibernateapp/entity/Factura.java
+++ hibernate-jpa-asociaciones-app/src/main/java/org/juliangranda/hibernateapp/entity/Factura.java
@@ -2,6 +2,8 @@ package org.juliangranda.hibernateapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name="facturas")
 public class Factura {
@@ -67,5 +69,18 @@ public class Factura {
                 ", descripcion='" + descripcion + '\'' +
                 ", total=" + total +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Factura factura = (Factura) o;
+        return Objects.equals(id, factura.id) && Objects.equals(descripcion, factura.descripcion) && Objects.equals(total, factura.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descripcion, total);
     }
 }

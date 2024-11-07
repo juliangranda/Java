b98b1753cb21b3cef5a815e3e50fe6e9e839725e
juliangranda <juliangranda@hotmail.com>
2024-11-07 11:04:03 -0500

    Fetch en anotaciones @OneToMany y @OneToOne usando ResultList

---
+++ hibernate-jpa-asociaciones-app/src/main/java/org/juliangranda/hibernateapp/HibernateFetchResultList.java
@@ -0,0 +1,17 @@
+package org.juliangranda.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.juliangranda.hibernateapp.entity.Cliente;
+import org.juliangranda.hibernateapp.util.JpaUtil;
+
+import java.util.List;
+
+public class HibernateFetchResultList {
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManager();
+        List<Cliente> clientes = em.createQuery("select distinct c from Cliente c left outer join fetch c.direcciones left outer join fetch c.detalle", Cliente.class).getResultList();
+
+        clientes.forEach(c -> System.out.println(c.getNombre() + ", direcciones: " + c.getDirecciones()));
+        em.close();
+    }
+}

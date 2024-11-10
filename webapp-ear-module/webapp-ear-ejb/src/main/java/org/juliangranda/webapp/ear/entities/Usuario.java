dfea95003b8eef1e39ad2d18c5e01dd8c9c389e2
juliangranda <juliangranda@hotmail.com>
2024-11-10 11:32:35 -0500

    Configurando DataSource en servidor Wildfly y persistencia JPA en el modulo ejb

---
+++ webapp-ear-module/webapp-ear-ejb/src/main/java/org/juliangranda/webapp/ear/entities/Usuario.java
@@ -0,0 +1,57 @@
+package org.juliangranda.webapp.ear.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

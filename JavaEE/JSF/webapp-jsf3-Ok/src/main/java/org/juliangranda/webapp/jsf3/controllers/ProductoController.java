860ea98e665d86efbb65bf6614850d2d9f490a9d
juliangranda <juliangranda@hotmail.com>
2024-11-13 17:09:31 -0500

    Form CRUD completo editar y eliminar

--- JavaEE/JSF/webapp-jsf3-Ok/src/main/java/org/juliangranda/webapp/jsf3/controllers/ProductoController.java
+++ JavaEE/JSF/webapp-jsf3-Ok/src/main/java/org/juliangranda/webapp/jsf3/controllers/ProductoController.java
@@ -15,6 +15,8 @@ public class ProductoController {
 
     private Producto producto;
 
+    private Long id;
+
     @Inject
     private ProductoService service;
 
@@ -33,14 +35,37 @@ public class ProductoController {
 
     @Produces
     @Model
-    public Producto producto(){
+    public Producto producto() {
         this.producto = new Producto();
+        if (id != null && id > 0) {
+            service.porId(id).ifPresent(p -> {
+                this.producto = p;
+            });
+        }
         return producto;
+    }
+
+    public String editar(Long id){
+        this.id = id;
+        return "form.xhtml";
     }
 
     public String guardar(){
         System.out.println(producto);
         service.guardar(producto);
         return "index.xhtml?faces-redirect=true";
+    }
+
+    public String eliminar(Long id){
+        service.eliminar(id);
+        return "index.xhtml?faces-redirect=true";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }

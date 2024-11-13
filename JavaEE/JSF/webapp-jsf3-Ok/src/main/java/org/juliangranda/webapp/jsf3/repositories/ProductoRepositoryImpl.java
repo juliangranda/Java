eecfa19eb17395932f23ba69bc805949b689283a
juliangranda <juliangranda@hotmail.com>
2024-11-12 21:21:52 -0500

    commit de seguridad de seccion inicial de JSF que funciona correctamente

---
+++ JavaEE/JSF/webapp-jsf3-Ok/src/main/java/org/juliangranda/webapp/jsf3/repositories/ProductoRepositoryImpl.java
@@ -0,0 +1,25 @@
+package org.juliangranda.webapp.jsf3.repositories;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.juliangranda.webapp.jsf3.entities.Producto;
+
+import java.util.List;
+
+@RequestScoped
+public class ProductoRepositoryImpl implements CrudRepository<Producto>{
+
+    @Inject
+    private EntityManager em;
+
+    @Override
+    public List<Producto> listar() {
+        return em.createQuery("select p from Producto p", Producto.class).getResultList();
+    }
+
+    @Override
+    public Producto porId(Long id) {
+        return em.find(Producto.class, id);
+    }
+}

762645d93444754708589906a53eda8eccc9df7d
juliangranda <juliangranda@hotmail.com>
2024-11-20 15:47:40 -0500

    añadiendo reglas de seguridad url y metodos http en archivo web.xml

---
+++ JAAS/webapp-jaxrs-jaas/src/main/java/org/juliangranda/webapp/jaxws/repositories/CursoRepository.java
@@ -0,0 +1,12 @@
+package org.juliangranda.webapp.jaxws.repositories;
+
+import org.juliangranda.webapp.jaxws.models.Curso;
+
+import java.util.List;
+
+public interface CursoRepository {
+    List<Curso> listar();
+    Curso guardar(Curso curso);
+    Curso porId(Long id);
+    void eliminar(Long id);
+}

4f4c6782f62ff3be34702e2d8d3c6b5c2e195278
juliangranda <juliangranda@hotmail.com>
2024-11-21 17:19:29 -0500

    El objeto Model

--- spring/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/indexController.java
+++ spring/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/indexController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
 //RequestMapping=ruta base que tienen en comun para todos los controladores.
 
 @Controller
@@ -18,5 +20,19 @@ public class indexController {
 	public String index(Model model) {
 		model.addAttribute("titulo","Hola Spring Framework con Model");
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Julian");
+		usuario.setApellido("Granda");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo","Perfil del usuario: "
+				.concat(usuario.getNombre()));
+		
+		return "perfil";
 	}
 }

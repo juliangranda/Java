84849d486039ed0c6ced06e792b9a01651d0d3e1
juliangranda <juliangranda@hotmail.com>
2024-11-21 16:13:59 -0500

    Pasar datos a la vista

--- spring/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/indexController.java
+++ spring/workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controller/indexController.java
@@ -1,6 +1,9 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,8 +11,8 @@ public class indexController {
 
 	//@RequestMapping(value="/index", method = RequestMethod.GET)
 	@GetMapping({"/index","/","/home"})
-	public String index() {
-		
+	public String index(Model model) {
+		model.addAttribute("titulo","Hola Spring Framework con Model");
 		return "index";
 	}
 }

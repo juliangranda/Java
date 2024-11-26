49e2ad33f0be1a0534bf3d14310da4846d6d8bdd
juliangranda <juliangranda@hotmail.com>
2024-11-26 16:32:30 -0500

    Registrando componente ItemsFactura

--- spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.bolsadeideas.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -20,5 +25,17 @@ public class AppConfig {
 	@Bean("miServicioComplejo")
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
+	}
+	
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems(){
+		Producto producto1 = new Producto("camara sony", 100);
+		Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto1, 4);
+		
+		return Arrays.asList(linea1,linea2);
+		
 	}
 }
--- spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
+++ spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Producto.java
@@ -3,8 +3,12 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 public class Producto {
 
 	private String nombre;
-
 	private Integer precio;
+
+	public Producto(String nombre, Integer precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
 
 	public String getNombre() {
 		return nombre;

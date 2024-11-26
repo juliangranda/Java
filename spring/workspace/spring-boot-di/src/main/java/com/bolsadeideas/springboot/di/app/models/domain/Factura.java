de208d9a87b6a2d2f3bd8961448b24b2571ff825
juliangranda <juliangranda@hotmail.com>
2024-11-26 17:32:04 -0500

    Registrando otras lineas para la factura

--- spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 

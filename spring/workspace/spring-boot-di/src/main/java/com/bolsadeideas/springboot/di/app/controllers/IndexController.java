09ddebb5bc85944d52b5700a93cf8dd21cac6783
juliangranda <juliangranda@hotmail.com>
2025-04-25 16:11:09 -0500

    añado algunos comentario por el tema de inyeccion de depencias en spring

--- spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ spring/workspace/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -11,6 +11,9 @@ import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 @Controller
 public class IndexController {
 	
+	//implementar el service,autowired ayuda a desacoplar el servicio de la app,
+	//siempre y cuando este registrado en el contenedor de spring.
+	//Qualifier: permite escoger el servio/componente en especifico a utilizar.
 	@Autowired
 	@Qualifier("miServicioComplejo")
 	private IServicio servicio;

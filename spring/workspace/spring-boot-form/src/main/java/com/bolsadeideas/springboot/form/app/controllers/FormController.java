790d2db29f7805a13b6fa29a5d0420b616708ad1
juliangranda <juliangranda@hotmail.com>
2025-04-29 16:51:50 -0500

    uso de @sessionAtributes para persistir datos haciendo uso de la sesion http

--- spring/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -7,24 +7,30 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
+		usuario.setNombre("Jhon");
+		usuario.setApellido("Doe");
+		usuario.setIdentificador("123.456.789-k");
 		model.addAttribute("titulo", "Formulario usuarios");
 		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		
@@ -34,8 +40,9 @@ public class FormController {
 			result.getFieldErrors().forEach(err ->{
 				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
 			});
-			model.addAttribute("error", errores);
-			return "form";*/
+			model.addAttribute("error", errores);*/
+			status.setComplete();
+			return "form";
 		}
 		
 		model.addAttribute("usuario", usuario);

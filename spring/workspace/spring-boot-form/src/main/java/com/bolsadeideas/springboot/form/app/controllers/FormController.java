dba710faaabc801574b2ab666f3865c69de28e48
juliangranda <juliangranda@hotmail.com>
2025-04-29 10:13:14 -0500

    manejo de formularios con thymeleaf y spring

--- spring/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
@@ -16,15 +16,10 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Model model, 
-			@RequestParam(name="username") String username,
-			@RequestParam String password,
-			@RequestParam String email) {
+	public String procesar(Usuario usuario, Model model){
 		
 		model.addAttribute("titulo", "Resultado form");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
+		model.addAttribute("usuario", usuario);
 
 		return "resultado";
 	}
